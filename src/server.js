dee58caf2c4f075720f3480018675810d78607b2
404thuy <24001647@eaut.edu.vn>
2026-04-30 06:43:36 +0700

    Include views and public in Vercel bundle and fix pathing

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "..", "views"));
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(process.cwd(), "views"));
+app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
